ce65a8ee347f6fde2455460e12a3fbe7fc066f9e
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-09-03 01:03:41 -0700

    Added security patch for superagent

--- build.ps1
+++ build.ps1
@@ -2,4 +2,5 @@
 & java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/speech -l javascript -o client -c packageconfig.json
 (Get-Content ./client/src/api/RecognizeApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/RecognizeApi.js
 (Get-Content ./client/src/api/SpeakApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/SpeakApi.js
+(Get-Content ./client/package.json).replace('"superagent": "3.5.2"', '"superagent": "3.7.0"') | Set-Content ./client/package.json
 & npm build ./client
--- client/src/model/SpeechRecognitionResult.js
+++ client/src/model/SpeechRecognitionResult.js
@@ -36,7 +36,7 @@
   /**
    * The SpeechRecognitionResult model module.
    * @module model/SpeechRecognitionResult
-   * @version 1.1.2
+   * @version 1.1.3
    */
 
   /**
